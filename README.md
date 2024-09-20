696b5b0ff2118c52d17aa38053b53bf877406d22
andreluke <deco99885@gmail.com>
2024-09-20 11:45:59 -0300

    build: Criação da primeira página do projeto

---
+++ README.md
@@ -0,0 +1,2 @@
+# in.orbit-Frontend
+Repositório para guardar o código do projeto de uma aplicação full stack (A parte do frontend da mesma) de controle de metas feita inspirada na NLW intermediaria
---
+++ src/app.tsx
@@ -0,0 +1,27 @@
+import { Plus } from "lucide-react";
+import logo from "./assets/logo-in-orbit.svg";
+import letsStart from "./assets/lets-start-illustration.svg";
+import { Button } from "./components/ui/button";
+
+export function App() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="lets start!" />
+      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+        Você ainda não cadastrou nenhuma meta! Quer começar agora mesmo?
+      </p>
+      <Button>
+        <Plus className="size-4" />
+        Cadastrar meta
+      </Button>
+      {/* <button
+        type="button"
+        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50  flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
+      >
+        <Plus className="size-4" />
+        Cadastrar meta
+      </button> */}
+    </div>
+  );
+}
