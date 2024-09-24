67b965340529ac2642a7134c2f46151c6ba1e9d2
andreluke <deco99885@gmail.com>
2024-09-24 11:04:13 -0300

    :construction: feat: Cadastro de metas

--- src/app.tsx
+++ src/app.tsx
@@ -1,27 +1,43 @@
-import { Plus } from "lucide-react";
+import { Plus, X } from "lucide-react";
 import logo from "./assets/logo-in-orbit.svg";
 import letsStart from "./assets/lets-start-illustration.svg";
 import { Button } from "./components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "./components/ui/dialog";
+import { Label } from "./components/ui/label";
+import { Input } from "./components/ui/input";
+import {
+  RadioGroup,
+  RadioGroupIndicator,
+  RadioGroupItem,
+} from "./components/ui/radio-group";
+import { CreateGoal } from "./components/create-goal";
 
 export function App() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="in.orbit" />
-      <img src={letsStart} alt="lets start!" />
-      <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta! Quer começar agora mesmo?
-      </p>
-      <Button>
-        <Plus className="size-4" />
-        Cadastrar meta
-      </Button>
-      {/* <button
-        type="button"
-        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50  flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
-      >
-        <Plus className="size-4" />
-        Cadastrar meta
-      </button> */}
-    </div>
+    <Dialog>
+      <div className="h-screen flex flex-col items-center justify-center gap-8">
+        <img src={logo} alt="in.orbit" />
+        <img src={letsStart} alt="lets start!" />
+        <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
+          Você ainda não cadastrou nenhuma meta! Quer começar agora mesmo?
+        </p>
+
+        <DialogTrigger>
+          <Button>
+            <Plus className="size-4" />
+            Cadastrar meta
+          </Button>
+        </DialogTrigger>
+      </div>
+
+      <CreateGoal />
+    </Dialog>
   );
 }
